54c77168e844577688d8cab05fdfac95605bacab
andrevinicius201 <andre.vinicius1899@gmail.com>
2024-02-20 17:01:37 -0300

    initial version

---
+++ src/http/server.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+import cookie from '@fastify/cookie'
+import { pollResults } from './ws/poll-results'
+import WebSocket from '@fastify/websocket'
+
+const app = fastify()
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+app.register(cookie, {
+    secret: "polls-app",
+    hook: "onRequest",
+})
+app.register(WebSocket);
+app.register(pollResults)
+
+app.listen({port: 3333}).then(() => {
+    console.log('HTTP Server Running')
+})
